01b3049c562516d829f84c2b62f272744a0dfcfb
Williams Ozuem <ozuemdw@gmail.com>
2021-03-24 22:51:08 +0100

    enabled dotenv

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(bodyParser.json());
